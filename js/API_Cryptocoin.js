62fc773eaaa03e77e6effb82afa22f5125dce586
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2021-08-26 22:43:49 -0300

    finishing initial interface

--- js/API_Cryptocoin.js
+++ js/API_Cryptocoin.js
@@ -1,33 +1,36 @@
-            //My api key
-            var apikey = {
-                key: '90b35aa9-e7d7-44e9-a608-08c3fb1a7545'
-            }
+import api from './key.js'
 
-        //GET Fetch Requisition
-        fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' +
-                apikey.key)
-                .then((response) => {
-                    if(!response.ok) throw new Error('Erro ao executar a requisição, status ' + response.status);
+/*  YOUR KEY HERE
+        const api = {
+            key: _____________
+        }
+*/
+
+//GET Fetch Requisition
+ fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='+api.key)
+     .then((response) => {
+        if(!response.ok) throw new Error('Erro ao executar a requisição, status ' + response.status);
                     return response.json();
             })
-            .then((api) => {
-                api.data.sort((a,b) => (a.rank > b.rank) ? 1 : ((b.rank > a.rank) ? -1 : 0))
-                console.log(api.data)
-                var texto = "";
-                for(let i = 0; i < 12; i++){
-                   let date = new Date(api.data[i].first_historical_data)
-                   date = date.toLocaleDateString("pt-BR")
+     .then((api) => {
+        api.data.sort((a,b) => (a.rank > b.rank) ? 1 : ((b.rank > a.rank) ? -1 : 0))
+        let texto = "";
+        for(let i = 0; i < 20; i++){
+                   let date = new Date(api.data[i].first_historical_data).toLocaleDateString("pt-BR")
                    let symbol = api.data[i].symbol
+                   let iconSymbol = symbol.toLowerCase()
+                   let urlIcon = `../cryptocurrency-icons-master/128/color/${iconSymbol}.png`
                    let stats = `https://coinmarketcap.com/pt-br/currencies/${api.data[i].slug}/`
-                  console.log(symbol)
                    texto = texto + `
                     
-                   <div class="card" style="width: 18rem; height: 26rem;">
-                   <img src="../img/${api.data[i].name}.jpg" class="card-img-top" alt="${api.data[i].name} style="height= 100%;">
-                   <div class="card-body">    
+            <div class="card" style="width: 18rem; height: 26rem; box-shadow: 0.4em 0.5em 0.7em rgba(0,0,0,0.19) ">
+                   <img src="../img/${api.data[i].symbol}.jpg" class="card-img-top" alt="${api.data[i].name} style="height= 260rem;">
+                   <div class="card-body" style="padding:.1em .2em;">    
                      <h5 class="card-title">${api.data[i].name} <span>${api.data[i].symbol}</span></h5>
                      <p class="card-text"><strong>Primeira data de aparecimento:</strong>
-                        <li>${date}</li>
+                        <li style="list-style: none;">
+                        <img src=${urlIcon} style="height:1.5rem; display: inline; margin-right: 3rem;">
+                        ${date}</li>
                      </p>
                      <div class="card__footer">
                      <a href="${stats}" class="btn btn-primary">Preço e estatísticas</a> 
@@ -41,5 +44,3 @@
             .catch((error) => {
                 console.error(error.message);
             });
-
-// <img class="icon" src="../cryptocurrency-icons-master/128/black/${symbol}.png">  
