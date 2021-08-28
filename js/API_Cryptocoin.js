fbab745457014c60ddd457cad3745ca8a5a3c590
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2021-08-28 08:47:59 -0300

    removendo estilos inline

--- js/API_Cryptocoin.js
+++ js/API_Cryptocoin.js
@@ -1,5 +1,4 @@
 import api from "./key.js";
-
 /*  YOUR KEY HERE
         const api = {
             key: _____________
@@ -18,30 +17,29 @@ fetch(
     return response.json();
   })
   .then((api) => {
-    api.data.sort((a, b) => (a.rank > b.rank ? 1 : b.rank > a.rank ? -1 : 0));
+    api.data.sort((a, b) => a.rank - b.rank);
     let texto = "";
     for (let i = 0; i < 20; i++) {
-      let date = new Date(api.data[i].first_historical_data).toLocaleDateString(
-        "pt-BR"
-      );
-      let symbol = api.data[i].symbol;
-      let iconSymbol = symbol.toLowerCase();
+      
+      const coin = {
+        symbol:api.data[i].symbol,
+        stats: `https://coinmarketcap.com/pt-br/currencies/${api.data[i].slug}/`,
+        date: new Date(api.data[i].first_historical_data).toLocaleDateString("pt-BR"),
+        name: api.data[i].name
+      }
+      let iconSymbol = coin.symbol.toLowerCase();
       let urlIcon = `../cryptocurrency-icons-master/128/color/${iconSymbol}.png`;
-      let stats = `https://coinmarketcap.com/pt-br/currencies/${api.data[i].slug}/`;
-      texto =
-        texto +
-        `
-                    
-            <div class="card" style="width: 18rem; height: 26rem; box-shadow: 0.4em 0.5em 0.7em rgba(0,0,0,0.19) ">
-                   <img src="../img/${api.data[i].symbol}.jpg" class="card-img-top" alt="${api.data[i].name} style="height= 260rem;">
+      
+        texto +=`<div class="card">
+                   <img src="../img/${coin.symbol}.jpg" class="card-img-top" alt="${coin.name} style="height= 260rem;">
                    <div class="card-body" style="padding:.1em .2em;">    
-                     <h5 class="card-title">${api.data[i].name} <span>${api.data[i].symbol}</span></h5>
+                     <h5 class="card-title">${coin.name} <span>${coin.symbol}</span></h5>
                      <ul class="card-text"><strong>Primeira data de aparecimento:</strong>
                         <li style="list-style: none;">
-                        <img src=${urlIcon} style="height:1.5rem; display: inline; margin-right: 3rem;">
-                        ${date}</li>
+                        <img src=${urlIcon} class="coin-icon">
+                        ${coin.date}</li>
                      </ul>
-                     <a href="${stats}" class="btn btn-primary">Preço e estatísticas</a>               
+                     <a href="${coin.stats}" class="btn btn-primary">Preço e estatísticas</a>               
                    </div>
                  </div>
                      `;
